0755363a5616d399f8af352d6bb7be98dd9f865b
Andy Casey <andycasey@gmail.com>
2018-05-30 15:38:12 -0400

    add model

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import stan_utils as stan
+from mpl_utils import (mpl_style, common_limits)
+
+
+plt.style.use(mpl_style)
 
 
 def generate_data(N, M, D, scales=None, seed=None):
@@ -44,8 +49,6 @@ def generate_data(N, M, D, scales=None, seed=None):
         scales,
         size=(N, D))
 
-    theta = np.random.normal(0, 1, size=(D, M))
-
     # TODO: Better way to randomly generate positive semi-definite covariance
     #       matrices that are *very* close to an identity matrix.
 
@@ -59,26 +62,104 @@ def generate_data(N, M, D, scales=None, seed=None):
     # TODO: use matrix multiplication you idiot
     theta = np.array([np.dot(L[i], L[i].T) for i in range(M)])
 
-
-
-    raise a
-
-    # TODO: be smrt
-    y = np.zeros((N, M, D), dtype=float)
-    for i in range(N):
-        y[i] = X[i] * theta.T
+    y = np.dot(X, theta)
 
     # add noise.
-    phi = np.abs(np.random.normal(0, 1, size=(M, D)))
-    rank = scales * np.random.normal(0, 1, size=y.shape)
+    phi = np.abs(np.random.normal(0, 0.1, size=(M, D)))
+    rank = np.random.normal(0, 1, size=y.shape)
 
-    noise = rank * phi 
+    noise = scales * rank * phi 
 
     y += noise
 
-    truths = dict(X=X, theta=theta.T, phi=phi.T, scales=scales)
+    truths = dict(X=X, theta=theta, phi=phi, scales=scales, L=L, noise=noise)
 
     return (y, truths)
 
 
-generate_data(1000, 3, 5)
+N, M, D = (250, 10, 5)
+scales =  np.ones(D)
+
+y, truths = generate_data(N=N, M=M, D=D, scales=scales)
+
+model = stan.read_model("model.stan")
+
+
+# Optimize the model
+data = dict(y=y, N=N, M=M, D=D, scales=scales)
+
+# TODO: initialize from true value.
+op_kwds = dict(
+    data=data, 
+    iter=100000, 
+    tol_obj=7./3 - 4./3 - 1, # machine precision
+    tol_grad=7./3 - 4./3 - 1, # machine precision
+    tol_rel_grad=1e3,
+    tol_rel_obj=1e4
+)
+
+
+p_opt = model.optimizing(**op_kwds)
+
+
+
+fig, axes = plt.subplots(1, D, figsize=(4 * D, 4))
+for d, ax in enumerate(axes):
+
+    x = truths["X"].T[d]
+    y = p_opt["X"].T[d]
+
+    ax.scatter(x, y)
+
+    ax.set_xlabel(r"$X_{{{0},\textrm{{true}}}}$".format(d))
+    ax.set_ylabel(r"$X_{{{0},\textrm{{opt}}}}$".format(d))
+
+    common_limits(ax, plot_one_to_one=True)
+
+fig.tight_layout()
+
+
+
+fig, axes = plt.subplots(1, D, figsize=(4 * D, 4))
+for d, ax in enumerate(axes):
+
+    x = truths["phi"].T[d]
+    y = p_opt["phi"][d]
+
+    ax.scatter(x, y)
+
+    ax.set_xlabel(r"$\phi_{{{0},\textrm{{true}}}}$".format(d))
+    ax.set_ylabel(r"$\phi_{{{0},\textrm{{opt}}}}$".format(d))
+
+    common_limits(ax, plot_one_to_one=True)
+
+fig.tight_layout()
+
+
+
+
+K = int(np.ceil(M**0.5))
+L = int(np.ceil(M / K))
+
+fig, axes = plt.subplots(K, L, figsize=(4 * L, 4 * K))
+axes = np.array(axes).flatten()
+
+for m, ax in enumerate(axes[:M]):
+
+    x = truths["theta"][m].flatten()
+    y = p_opt["theta"][m].flatten()
+
+    ax.scatter(x, y)
+
+    ax.set_xlabel(r"$\theta_{{{0},\textrm{{true}}}}$".format(m))
+    ax.set_ylabel(r"$\theta_{{{0},\textrm{{opt}}}}$".format(m))
+    
+    common_limits(ax, plot_one_to_one=True)
+
+for ax in axes[M:]:
+    ax.set_visible(False)
+
+fig.tight_layout()
+
+
+
---
+++ model.stan
@@ -0,0 +1,77 @@
+
+// Latent factor model for chemical abundances from multiple studies
+
+data {
+  int<lower=1> N; // number of stars
+  int<lower=1> D; // dimensionality of the data (number of labels)
+  int<lower=1> M; // number of surveys (or studies)
+  vector[D] y[N, M]; // the labels as reported by various surveys.
+  vector<lower=0>[D] scales; // fixed relative scales for latent factors
+}
+
+transformed data {
+  vector[D] mu; // the mean of the data in each dimension
+  int<lower=1> Q; // the number of non-zero lower-triangular entries that we
+                  // need for the decomposoition of our theta matrix
+  Q = M * choose(D, 2);
+
+  // TODO: Stop assuming that the user is not an idiot
+  mu = rep_vector(0.0, D);
+}
+
+parameters {
+  vector[D] X[N]; // latent factors for each star
+  vector<lower=0>[M] phi[D]; // variance on survey labels
+
+  vector[Q] L_lower_triangular; // lower triangular entries of the decomposition
+                                // of the  theta matrix
+  vector<lower=0>[M] L_diag[D]; // diagonal entries of the decomposition of the 
+                                // theta matrix
+}
+
+transformed parameters {
+  cholesky_factor_cov[D, D] L[M];
+  matrix[D, D] theta[M];
+  {
+    int q = 0;
+
+    for (m in 1:M)
+      for (i in 1:D)
+        for (j in (i + 1):D) 
+          L[m, i, j] = 0.0;
+
+    for (m in 1:M) {
+      for (i in 1:D) {
+        L[m, i, i] = L_diag[i, m];
+        for (j in (i + 1):D) {
+          q = q + 1;
+          L[m, j, i] = L_lower_triangular[q];
+        }
+      }
+    }
+
+    for (m in 1:M)
+      theta[m] = multiply_lower_tri_self_transpose(L[m]);
+  }
+}
+
+model {
+
+  // Place priors on various properties.
+  for (d in 1:D) {
+    X[:, d] ~ normal(rep_vector(0, N), rep_vector(scales[d], N));
+    phi[d] ~ normal(rep_vector(0, M), rep_vector(1, M));
+
+    // TODO: Should we be placing an inverted Wishart prior or something on this
+    //       shit?
+    //L_lower_triangular
+
+    // TODO: revisit this prior
+    L_diag[d] ~ normal(rep_vector(1, M), rep_vector(0.1, M));
+  }
+
+  for (n in 1:N) 
+    for (m in 1:M)
+      y[n, m, :] ~ normal(to_row_vector(X[n]) * theta[m], phi[:, m]);
+}
+
