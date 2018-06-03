1c4528eb87eeba2a76d1104878b25e391657ec5d
Andy Casey <andycasey@gmail.com>
2018-06-03 11:58:09 -0400

    build paper

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -2,10 +2,11 @@
 
 # Push to GitHub
 if [ -n "$GITHUB_API_KEY" ]; then
+  print "pushing to github"
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan pdf
   git rm -rf .
   git add -f paper/uberchemical.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG pdf
+  git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/andycasey/uber-chemi-cal pdf
 fi
